051b3b522e37e5ec1c295208f1960f386915953b
Dagurasu56 <douglas_search@hotmail.com>
2022-04-08 08:38:52 -0300

    editando, deletando e buscando alunos

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"github.com/Dagurasu56/go-gin-api/database"
-	"github.com/Dagurasu56/go-gin-api/models"
 	"github.com/Dagurasu56/go-gin-api/routes"
 )
 
 func main() {
 
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Théo Souza", CPF: "176.123.123-12", RG: "38000000"},
-		{Nome: "Débora Souza", CPF: "123.533.123-12", RG: "54000000"},
-	}
 	routes.HandleRequests()
 }
