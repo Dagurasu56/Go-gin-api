52a29e5b16cc569412aae5d6d3456ece572c5331
Dagurasu56 <douglas_search@hotmail.com>
2022-04-08 14:43:08 -0300

    testando os endpoints

--- main_test.go
+++ main_test.go
@@ -8,15 +8,30 @@ import (
 	"testing"
 
 	"github.com/Dagurasu56/go-gin-api/controllers"
+	"github.com/Dagurasu56/go-gin-api/database"
+	"github.com/Dagurasu56/go-gin-api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
 }
 
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Dagurasu", CPF: "34526776412", RG: "890765431"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
+}
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	r := SetupDasRotasDeTeste()
 	r.GET("/:nome", controllers.Saudacao)
@@ -25,10 +40,36 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	r.ServeHTTP(resposta, req)
 
 	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
-	mockDaResposta := `{"API diz:":"E ai doug, tudo beleza?"}`
+	mockDaResposta := `{"API diz":"E ai doug, tudo beleza?"}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
 
 	fmt.Println(string(respostaBody))
 	fmt.Println(mockDaResposta)
+}
+
+func TestListandoTodosAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/35487614704", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
